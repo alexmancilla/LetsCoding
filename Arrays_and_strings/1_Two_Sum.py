183723acc050e0b7041dae5fc2611d01d92f3bfe
alexmancilla <alexmancilla.dev@gmail.com>
2023-06-05 17:40:08 -0600

    two sum

--- Arrays_and_strings/1_Two_Sum.py
+++ Arrays_and_strings/1_Two_Sum.py
@@ -2,26 +2,40 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-nums = [2,11,15]
+nums = [2,2,7,11,15]
 
 def getTwoSum(nums: list[int], target: int) -> list[int]:
     # n = len(nums)
-    # for i in range(n - 1 ):
-    #     for j  in range(i + 1, n):
-            # if nums[i] + nums[j] == target:
-            #     print(nums[i], nums[j])
-    l = 0
-    r = len(nums) - 1
-    while l <= r:
-        if nums[l] + nums[r] == target:
-            print(nums[l], nums[r])
-        elif nums[l] + nums[r] > target:
-            r -= 1
-        else: 
-            l += 1
-    print([])
-        
+    # for i in range(n - 1):
+    #     for j in range(i + 1, n):
+    #         if nums[i] + nums[j] == target:
+    #             return [nums[i],nums[j]]
+    # return []
 
-getTwoSum(nums, 9)
+
+    # l, r = 0, len(nums) - 1
+    # while l <= r:
+    #     if nums[l] + nums[r] == target:
+    #         return [nums[l], nums[r]]
+    #     elif nums[l] + nums[r] > target:
+    #         r -= 1
+    #     else:
+    #         l += 1
+
+    # return []
+
+    hashmap = {}
+    n = len(nums)
+    
+    for element in range(n):
+        diff = target - nums[element]
+        if diff in hashmap:
+            return [hashmap[element], element]
+        else:
+            hashmap[nums[element]] = element
+            
+
+result = getTwoSum(nums, 9)
+print(result)
 
 

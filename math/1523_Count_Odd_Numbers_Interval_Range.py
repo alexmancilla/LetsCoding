020862ec747157638ed166d703c10a013d2117ef
alexmancilla <alexmancilla.dev@gmail.com>
2023-06-10 10:02:07 -0600

    Se corrigio el error en el archivo 1523_Count_Odd_Numbers_Interval_Range.py

--- math/1523_Count_Odd_Numbers_Interval_Range.py
+++ math/1523_Count_Odd_Numbers_Interval_Range.py
@@ -8,15 +8,17 @@ Input: low = 3, high = 7
 Output: 3
 Explanation: The odd numbers between 3 and 7 are [3,5,7].
 """
-low = 3
-high = 7
+low = 21
+high = 22
 
 def countOddNums(low: int, high: int)-> int:
     count = 0
-    for i in range(low, high + 1):
-        if i % 2 != 0:
-            count += 1
-    return count   
+    dif = (high - low) + 1 
+    count = dif // 2
+    if dif % 2 != 0:
+        if low % 2 != 0:
+            return count + 1
+    return count
 sol = countOddNums(low, high)
 print(sol)
 

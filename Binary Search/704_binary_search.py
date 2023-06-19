a88c392a130bd245bcbf2373324e3b2be3b6a4f1
alexmancilla <alexmancilla.dev@gmail.com>
2023-06-19 15:13:37 -0600

    new section added

---
+++ Binary Search/704_binary_search.py
@@ -0,0 +1,14 @@
+"""Given an array of integers nums which is sorted in ascending order, and an integer target, 
+write a function to search target in nums. 
+-If target exists, then return its index. 
+-Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity."""
+nums = [-1,0,3,5,9,12]
+target = 9
+
+def binarySearch(nums: list[int], target: int)->int:
+    pass
+
+sol = binarySearch(nums,target)
+print(sol)

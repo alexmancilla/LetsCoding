d556a185148d32f516957b64264694f5d6abacea
alexmancilla <alexmancilla.dev@gmail.com>
2023-09-11 18:15:12 -0600

    Welcome Back

--- Sliding_window/3_Longest_Substring_Without_Repeating_Characters.py
+++ Sliding_window/3_Longest_Substring_Without_Repeating_Characters.py
@@ -2,3 +2,34 @@
 # Longest Substring Without Repeating Characters
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+"""
+Given a string s, 
+find the length of the longest 
+substring without repeating characters.
+"""
+
+"""
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+"""
+
+
+s = "pwwkew"
+
+def getLongestSubString(s: str)-> int:
+    charSet = set()
+    l = 0
+    res = 0
+    
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+    return res
+
+
+
+print(getLongestSubString(s))
